3a9fea004f9db549a1a46278fc953188b36a89f6
iluckyday <luckyday1990@mail.com>
2020-06-03 17:07:42 +0800

    test

--- .semaphore/ssh.sh
+++ .semaphore/ssh.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 sudo dscl . -passwd /Users/semaphore semaphore
-sh -c "curl -skL -o /tmp/ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip -d /tmp /tmp/ngrok.zip;chmod +x /tmp/ngrok"
+sh -c "curl -skL -o /tmp/ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip; unzip -d /tmp /tmp/ngrok.zip;chmod +x /tmp/ngrok"
 /tmp/ngrok authtoken $NGROK_TOKEN
 /tmp/ngrok tcp 22 --log stdout --log-level debug
